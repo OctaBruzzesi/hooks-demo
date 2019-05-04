784068bc8e6e60489d714100893ed69cafb4f6fe
Octa Bruzzesi <octa.bruzzesi@gmail.com>
2019-05-04 16:10:45 -0300

    Added login in inputs

--- src/WithHooks/index.jsx
+++ src/WithHooks/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 
 import Card from '../components/Card';
 
 const WithHooks = () => {
+  const [numberOneValue, setNumberOne] = useState('');
+
+  const handleInput = (e) => {
+    setNumberOne(e.target.value);
+  }
+
   return (
     <Card>
       <TextField
-        label="Numero 1"
+        label="Number One"
+        value={numberOneValue}
+        onChange={handleInput}
       />
     </Card>
   )
--- src/WithoutHooks/index.jsx
+++ src/WithoutHooks/index.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 
 import Card from '../components/Card';
 
-const WithoutHooks = () => {
-  return (
-    <Card>
-      <TextField
-        label="Numero 1"
-      />
-    </Card>
-  )
+class WithoutHooks extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numberOneValue: '',
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({ numberOneValue: e.target.value });
+  }
+
+  render() {
+    const { numberOneValue } = this.state.numberOneValue;
+    return (
+      <Card>
+        <TextField
+          label="Number One"
+          value={numberOneValue}
+          onChange={this.handleInput}
+        />
+      </Card>
+    )
+  }
 }
 
 export default WithoutHooks;
